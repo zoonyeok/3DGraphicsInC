c17621d92daac48393f5c7f1eb92899dc2bbfdd2
zoonyeok <ryubb09@gmail.com>
2022-05-17 00:46:06 +0900

    Rotation

---
+++ MyProject/Main.c
@@ -0,0 +1,144 @@
+#include "display.h"
+#include "vector.h"
+
+#define N_POINTS 9 * 9 * 9
+vec3_t cube_points[N_POINTS]; // 
+vec2_t projected_points[N_POINTS];
+
+vec3_t camera_position = { 0.0f, 0.0f, -5.0f };
+vec3_t cube_rotation = { 0.0f, 0.0f, 0.0f};
+
+float fov_factor = 640;
+
+bool is_running = false;
+uint32_t previous_frame_time = 0;
+
+void setup(void) {
+	// Allocate the required memory in bytes to hold the color buffer
+	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
+
+	// Creating a SDL texture that is used to display the color buffer
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+
+	int point_count = 0;
+
+	// from -1 to 1 (int this 9 9 9 cube)
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = { .x = x, .y = y, .z = z };
+				cube_points[point_count++] = new_point;
+			}
+		}
+	}
+}
+
+void process_input(void) 
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) 
+	{
+	case SDL_QUIT:
+		is_running = false;
+		break;
+	case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE)
+			is_running = false;
+		break;
+	}
+}
+
+vec2_t project(vec3_t point)
+{
+	vec2_t projected_point = 
+	{
+		.x = (fov_factor * point.x) / point.z,
+		.y = (fov_factor * point.y) / point.z
+	};
+
+	return projected_point;
+}
+
+void update(void) 
+{
+	//while (!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
+	
+	// Wait some time until the reach the target frame time in milliseconds
+	int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+
+	// Only delay execution if we are running too fast
+	if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) 
+	{
+		SDL_Delay(time_to_wait);
+	}
+
+	previous_frame_time = SDL_GetTicks();
+
+	cube_rotation.x += 0.01f;
+	cube_rotation.y += 0.01f;
+	cube_rotation.z += 0.01f; 
+
+	for (int i = 0; i < N_POINTS; i++) 
+	{
+		vec3_t point = cube_points[i];
+
+		vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+		transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+		transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
+		// Translate the points away from the camera
+		transformed_point.z -= camera_position.z;
+
+		vec2_t projected_point = project(transformed_point);
+
+		projected_points[i] = projected_point;
+	}
+}
+
+void render(void) 
+{
+	draw_grid();
+
+	// loop all projected points and render them
+	for (int i = 0; i < N_POINTS; i++)
+	{
+		vec2_t projected_point = projected_points[i];
+		draw_rect
+		(
+			projected_point.x + (window_width / 2), 
+			projected_point.y + (window_height / 2), 
+			4, 4, 0xFFFFFF00
+		);
+	}
+
+	render_color_buffer();
+	clear_color_buffer(0xFF000000);
+
+	SDL_RenderPresent(renderer);
+}
+
+int main(void) 
+{
+	is_running = initialize_window();
+
+	setup();
+
+	while (is_running) 
+	{
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+
+	return 0;
+}
---
+++ MyProject/vector.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+	float x;
+	float y;
+} vec2_t;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+} vec3_t;
+
+//TODO : Add fuctions to manipulate vector2D and vector3D
+
+vec3_t vec3_rotate_x(vec3_t v, float angle);
+vec3_t vec3_rotate_y(vec3_t v, float angle);
+vec3_t vec3_rotate_z(vec3_t v, float angle);
+
+#endif // !VECTOR_H
