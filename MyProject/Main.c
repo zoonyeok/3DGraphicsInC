0b6d9634ed65985612c0fd698524af4e43fd7e20
zoonyeok <ryubb09@gmail.com>
2022-05-18 06:23:06 +0900

    Backface Culling

--- MyProject/Main.c
+++ MyProject/Main.c
@@ -6,7 +6,7 @@
 
 triangle_t* triangles_to_render = NULL;
 
-vec3_t camera_position = { 0.0f, 0.0f, -5.0f };
+vec3_t camera_position = { 0.0f, 0.0f, 0.0f };
 
 float fov_factor = 640;
 
@@ -28,7 +28,7 @@ void setup(void)
 	);
 
 	//load_cube_mesh_data();
-	load_obj_file_data2("ezreal.obj"); // ./assets/cube.obj
+	load_obj_file_data("cube.obj"); // ./assets/cube.obj
 }
 
 void process_input(void) 
@@ -59,6 +59,25 @@ vec2_t project(vec3_t point)
 	return projected_point;
 }
 
+bool backface_culling(vec3_t transformed_vertices[])
+{
+	vec3_t vector_a = transformed_vertices[0];	/*   A   */
+	vec3_t vector_b = transformed_vertices[1];	/*  / \  */
+	vec3_t vector_c = transformed_vertices[2];	/* C---B */
+
+	vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+	vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+
+	vec3_t normal = vec3_cross(vector_ab, vector_ac);
+	vec3_t vector_a_camera = vec3_sub(vector_a, camera_position);
+
+	if (vec3_dot(normal, vector_a_camera) > 0)
+	{
+		return false;
+	}
+	return true;
+}
+
 void update(void) 
 {
 	//while (!SDL_TICKS_PASSED(SDL_GetTicks(), previous_frame_time + FRAME_TARGET_TIME));
@@ -78,20 +97,24 @@ void update(void)
 	g_mesh2.rotation.y += 0.01f;
 	g_mesh2.rotation.z += 0.01f;
 
+
 	// 배열 초기화
 	triangles_to_render = NULL;
 
 	int num_faces = array_length(g_mesh2.faces);
 	// 모든 triangle face 순회
-	for (int i = 0; i < num_faces; i++) {
+	for (int i = 0; i < num_faces; i++) 
+	{
 		face_t mesh_face = g_mesh2.faces[i];
 		
 		vec3_t face_vertices[3];
-		face_vertices[0] = g_mesh2.vertices[mesh_face.a - 1];
-		face_vertices[1] = g_mesh2.vertices[mesh_face.b - 1];
-		face_vertices[2] = g_mesh2.vertices[mesh_face.c - 1];
+		face_vertices[0] = g_mesh2.vertices[mesh_face.a - 1]; 
+		face_vertices[1] = g_mesh2.vertices[mesh_face.b - 1]; 
+		face_vertices[2] = g_mesh2.vertices[mesh_face.c - 1]; 
 
 		triangle_t projected_triangle;
+
+		vec3_t transformed_vertices[3];
 
 		// loop all three vertices of this current face and apply transformations
 		for (int j = 0; j < 3; j++)
@@ -103,10 +126,32 @@ void update(void)
 			transformed_vertex = vec3_rotate_z(transformed_vertex, g_mesh2.rotation.z);
 
 			// Translate the vertex away from the camera
-			transformed_vertex.z -= camera_position.z;
+			transformed_vertex.z += 5; //inside monitor
 
+			// Save transfomed vertex in the arrya of transformed vertices
+			transformed_vertices[j] = transformed_vertex;
+		}
+
+		// TODO: Check backface culling
+		vec3_t vector_a = transformed_vertices[0];	/*   A   */
+		vec3_t vector_b = transformed_vertices[1];	/*  / \  */
+		vec3_t vector_c = transformed_vertices[2];	/* C---B */
+
+		vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+		vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+		vec3_t normal = vec3_cross(vector_ab, vector_ac);
+		vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+
+		float dot_normal_camera = vec3_dot(normal, camera_ray);
+
+		if (dot_normal_camera < 0)
+			continue;
+
+		// Loop all three vertices tor perform projection
+		for (int j = 0; j < 3; j++)
+		{
 			// Project the current vertex
-			vec2_t projected_point = project(transformed_vertex);
+			vec2_t projected_point = project(transformed_vertices[j]);
 
 			// Scale and translate the projected points to the middle of the screen
 			projected_point.x += (window_width / 2);
@@ -114,7 +159,6 @@ void update(void)
 
 			projected_triangle.points[j] = projected_point;
 		}
-
 		// save the projected triangle in the array of triangles to render
 		// triangles_to_render[i] = projected_triangle;
 		array_push(triangles_to_render, projected_triangle);
@@ -132,9 +176,9 @@ void render(void)
 	for (int i = 0; i < triangle_array_length; i++)
 	{
 		triangle_t triangle = triangles_to_render[i];
-		//draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, Yellow); // vertex A
-		//draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, Yellow); // vertex B
-		//draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, Yellow); // vertex C
+		draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, Yellow); // vertex A
+		draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, Yellow); // vertex B
+		draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, Yellow); // vertex C
 
 		draw_triangle(triangle, Green);
 	}
